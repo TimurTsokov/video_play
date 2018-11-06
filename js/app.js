32b270b58205f52fc5f7e27aa33d174873cc9418
TimurTsokov <tsokov2t1m5@gmail.com>
2018-11-06 19:15:58 +0200

    updatestr & closestr No work

--- js/app.js
+++ js/app.js
@@ -48,208 +48,231 @@
 //     request.send(device);
 // }
 
-$.ajax({
-    header: {
-        'Accept-Encoding': 'gzip',
-        'auth-key': 'value'
-    },
-    type: 'POST',
-    url: "https://tv-server.trinity-tv.net/server/TvServerService/Auth.json",
-    data: JSON.stringify({
-        device: {
-            mac: '34:FC:EF:D9:C4:B2',
-            type: 'DT_SmartTV',
-            sub_type: 'DST_TOSHIBA',
-            application: {
-                type: 'AT_SWEET_TV_Player'
-            },
-            locale: 'uk'
-        }
-    }),
+(function Auth() {
+    $.ajax({
+        header: {
+            'Accept-Encoding': 'gzip',
+            'auth-key': 'value'
+        },
+        type: 'POST',
+        url: "https://tv-server.trinity-tv.net/server/TvServerService/Auth.json",
+        data: JSON.stringify({
+            device: {
+                mac: '34:FC:EF:D9:C4:B2',
+                type: 'DT_SmartTV',
+                sub_type: 'DST_TOSHIBA',
+                application: {
+                    type: 'AT_SWEET_TV_Player'
+                },
+                locale: 'uk'
+            }
+        }),
 
-    dataType: 'json',
-    contentType: "application/json",
+        dataType: 'json',
+        contentType: "application/json",
 
-    success: function (status, response, request) {
-        var obj = JSON.parse(request.responseText);
-        console.log('Auth', obj);
-        if (status == 500) {
-            alert('Запрос неверно сформировани, ошибка сервера')
+        success: function (status, response, request) {
+            var obj = JSON.parse(request.responseText);
+            console.log('Auth', obj);
+            if (status == 500) {
+                alert('Error')
+            }
+        },
+        error: function (e) {
+            console.log(e);
         }
-    },
-    error: function (e) {
-        console.log(e);
-    }
-    // success: function (resp, status, xhr) {
-    //     var msg = "result is: " + resp.result + ", Start: " + resp.start + ", End: " + resp.end + ", issues: " + resp.issues;
-    //     jQuery("#successPost").html(msg + " - STATUS: " + xhr.status + " " + xhr.statusText);
-    // },
-    // error: function (resp, status, xhr) {
-    //     alert("Error: " + resp.e);
-    //     jQuery("#errorPost").html("Error: " + resp.e + " - STATUS: " + xhr.status + " " + xhr.statusText);
-    // }
-    // response: function () {}
-});
+        // success: function (resp, status, xhr) {
+        //     var msg = "result is: " + resp.result + ", Start: " + resp.start + ", End: " + resp.end + ", issues: " + resp.issues;
+        //     jQuery("#successPost").html(msg + " - STATUS: " + xhr.status + " " + xhr.statusText);
+        // },
+        // error: function (resp, status, xhr) {
+        //     alert("Error: " + resp.e);
+        //     jQuery("#errorPost").html("Error: " + resp.e + " - STATUS: " + xhr.status + " " + xhr.statusText);
+        // }
+        // response: function () {}
+    });
+
+})();
 
 //////////////////////////////////////////////////////////////////////////
+(function getChannels() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://tv-server.trinity-tv.net/server/TvServerService/GetChannels.json',
+        data: JSON.stringify({
+            auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
+            need_icons: true,
+            need_epg: true,
+            need_offsets: false,
+            need_categories: true,
+            epg_start_offset: 6,
+            epg_stop_offset: 7,
+            channels: 5,
+            category: 9
+        }),
+        dataType: 'json',
+        contentType: "application/json",
 
-$.ajax({
-    type: 'POST',
-    url: 'https://tv-server.trinity-tv.net/server/TvServerService/GetChannels.json',
-    data: JSON.stringify({
-        auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
-        need_icons: true,
-        need_epg: true,
-        need_offsets: false,
-        need_categories: true,
-        epg_start_offset: 6,
-        epg_stop_offset: 7,
-        channels: 5,
-        category: 9
-    }),
-    dataType: 'json',
-    contentType: "application/json",
-
-    // beforeSend: function(xhr){
-    //     var readystatehook = xhr.onreadystatechange;
-    //
-    //     xhr.onreadystatechange = function(){
-    //         readystatehook.apply(this, []);
-    //         console.log('ul');
-    //     };
-    // },
-    success: function (data) {
-        // data = JSON.parse(request.responseText);
-        console.log('GetChannels', data);
-        // var data = $(response).push($(list.arr[1]).html());
-        // var some = JSON.parse(data);
-        // for( var i=0; i<some.length; i++){
-        //     $('#successPost').text(list);
-        // }
-        // var channels = [];
-        // $('#successPost').text(data.name);
-        //  var  list = function (data) {
-        //     $('#successPost').append(data.list)
-        // }
-        // data = jQuery.parseJSON(data);
-        // $.each(data, function (index, item) {
-        //     $('.channels__list').append('<li>' + item.toString() + '<a>' + item.name + '</a></li>')
-        // });
-        // var out = '';
-        // for (var key in data) {
+        // beforeSend: function(xhr){
+        //     var readystatehook = xhr.onreadystatechange;
         //
-        //         out += '<ul class="channels__list">';
-        //         out += '<li><img src="' + data[key].list[0].icon_url + '" alt=""><h3>' + data[key].list[0].name + '</h3></li>';
-        //         out += '</ul>';
-        //
+        //     xhr.onreadystatechange = function(){
+        //         readystatehook.apply(this, []);
+        //         console.log('ul');
+        //     };
+        // },
+        success: function (data) {
+            // data = JSON.parse(request.responseText);
+            console.log('GetChannels', data);
+            // var data = $(response).push($(list.arr[1]).html());
+            // var some = JSON.parse(data);
+            // for( var i=0; i<some.length; i++){
+            //     $('#successPost').text(list);
+            // }
+            // var channels = [];
+            // $('#successPost').text(data.name);
+            //  var  list = function (data) {
+            //     $('#successPost').append(data.list)
+            // }
+            // data = jQuery.parseJSON(data);
+            // $.each(data, function (index, item) {
+            //     $('.channels__list').append('<li>' + item.toString() + '<a>' + item.name + '</a></li>')
+            // });
+            // var out = '';
+            // for (var key in data) {
+            //
+            //         out += '<ul class="channels__list">';
+            //         out += '<li><img src="' + data[key].list[0].icon_url + '" alt=""><h3>' + data[key].list[0].name + '</h3></li>';
+            //         out += '</ul>';
+            //
+            // }
+            // $('#result').html(out);
+
+            // for (var key2 in data[key])
+            //     for (var key3 in data[key][key2])
+            // $('.channels__list').append('<li><img src="${data[key].icon_url}">' + key + '' + data[key]+ '<a>' + ${data[key].name} + '</a></li>')
+
+            // $('.channels__list').append('<li><img src = "">' + data[key][key2].name + '</li>');
+
+            // $('.channels__list').html(data[1][0][9]);
+
+
+            // }
+            // $.each( data, function( key, value ) {
+            //     if($.inArray(list.id, data ) == 0){
+            //         $('.channels__list').append('<li>' + value.icon_url + ": " + value.name + '</li>');
+            //     }
+            // });
+            // var out = '';
+            // for (var icon_url in data) {
+            //     out += `<li><img src = ${data[icon_url]}></li>`
+            // }
+            // $('.channels__list').append(out);
+
+            var out = '';
+            out += '<ul class="channels__list">';
+            out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[0].icon_url + '" alt=""><h2>' + data.list[0].name + '</h2></a></li>';
+            out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[1].icon_url + '" alt=""><h2>' + data.list[1].name + '</h2></a></li>';
+            out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[2].icon_url + '" alt=""><h2>' + data.list[2].name + '</h2></a></li>';
+            out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[3].icon_url + '" alt=""><h2>' + data.list[3].name + '</h2></a></li>';
+            out += '</ul>';
+            $('#ch_list').html(out);
+            // console.log(data.list[0].name);
+        }
+        // error: function (e) {
+        //     console.log(e);
         // }
-        // $('#result').html(out);
 
-        // for (var key2 in data[key])
-        //     for (var key3 in data[key][key2])
-        // $('.channels__list').append('<li><img src="${data[key].icon_url}">' + key + '' + data[key]+ '<a>' + ${data[key].name} + '</a></li>')
+    });
+})();
 
-        // $('.channels__list').append('<li><img src = "">' + data[key][key2].name + '</li>');
-
-        // $('.channels__list').html(data[1][0][9]);
-
-
-        // }
-        // $.each( data, function( key, value ) {
-        //     if($.inArray(list.id, data ) == 0){
-        //         $('.channels__list').append('<li>' + value.icon_url + ": " + value.name + '</li>');
-        //     }
-        // });
-        // var out = '';
-        // for (var icon_url in data) {
-        //     out += `<li><img src = ${data[icon_url]}></li>`
-        // }
-        // $('.channels__list').append(out);
-
-        var out = '';
-        out += '<ul class="channels__list">';
-        out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[0].icon_url + '" alt=""><h2>' + data.list[0].name + '</h2></a></li>';
-        out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[1].icon_url + '" alt=""><h2>' + data.list[1].name + '</h2></a></li>';
-        out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[2].icon_url + '" alt=""><h2>' + data.list[2].name + '</h2></a></li>';
-        out += '<li class="channels__list-item"><a href="https://turbo.net/run/videolan/vlc" target="_blank"><img src="' + data.list[3].icon_url + '" alt=""><h2>' + data.list[3].name + '</h2></a></li>';
-        out += '</ul>';
-        $('#ch_list').html(out);
-
-        // console.log(data.list[0].name);
-    }
-    // error: function (e) {
-    //     console.log(e);
-    // }
-
-});
 
 ///////////////////////////////////////////////////////////////////
+(function openStream() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://tv-server.trinity-tv.net/server/TvServerService/OpenStream.json',
+        data: JSON.stringify({
+            auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
+            channel_id: parseInt(15),
+            offset_id: 3,
+            accept_scheme: 'HTTP_HLS',
+            first_batch_size: 5
 
-$.ajax({
-    type: 'POST',
-    url: 'https://tv-server.trinity-tv.net/server/TvServerService/OpenStream.json',
-    data: JSON.stringify({
-        auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
-        channel_id: parseInt(15),
-        offset_id: 3,
-        accept_scheme: 'HTTP_HLS',
-        first_batch_size: 5
+        }),
+        dataType: 'json',
+        contentType: "application/json",
 
-    }),
-    dataType: 'json',
-    contentType: "application/json",
+        success: function (data) {
+            console.log('OpenStream', data);
+            // $('#stream').text(data);
 
-    success: function (data) {
-        console.log('OpenStream', data);
-        // $('#stream').text(data);
+            // $('.channels__list-item').on('click', function () {
+            //
+            //     var str = '';
+            //
+            //     str += '<a href="">';
+            //     str += '<p>' + 'qwerty' + '</p>';
+            //     str += '</a>';
+            // str += '<source src="' +'http://'+ data.http_stream.host.address +':'+ data.http_stream.host.port + data.http_stream.url + '.m3u8' + '" type="video/mp4">';
+            // str += '</video>';
+            // // console.log(str);
+            // $('#stream').html(str);
+            // $('#stream').html(str);
+            // });
+            for (var i in data.update_interval) {
+                setInterval('updateStream()', data.update_interval[i]);
+            }
 
-        // $('.channels__list-item').on('click', function () {
-        //
-        //     var str = '';
-        //
-        //     str += '<a href="">';
-        //     str += '<p>' + 'qwerty' + '</p>';
-        //     str += '</a>';
-        // str += '<source src="' +'http://'+ data.http_stream.host.address +':'+ data.http_stream.host.port + data.http_stream.url + '.m3u8' + '" type="video/mp4">';
-        // str += '</video>';
-        // // console.log(str);
-        // $('#stream').html(str);
-        // $('#stream').html(str);
-        // });
+        }
+    });
 
-    }
-});
+})();
+
 // http://stream.mytrinity.com.ua:4021/stream/rb6bv97j43syj6zz2jtkk96tsvbvspue6m7qkx6e8yb38f5vpv637pprhrq55hf64d66we4duf4dcrx9qzbqcbjt9xb2sd73maapfpv69j3r35kt3cycbi3nxfc5wah82hvqss42d4xce3s.m3u8
 ////////////////////////////////////////////////
-$.ajax({
-    type: 'POST',
-    url: 'https://tv-server.trinity-tv.net/server/TvServerService/UpdateStream.json',
-    data: JSON.stringify({
-        auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
-        stream_id: 2
-    }),
-    dataType: 'json',
-    contentType: "application/json",
-    success: function (status, response, request) {
-        var obj = JSON.parse(request.responseText);
-        console.log('UpdateStream', obj);
-    }
-});
+(function updateStream() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://tv-server.trinity-tv.net/server/TvServerService/UpdateStream.json',
+        data: JSON.stringify({
+            auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
+            stream_id: 2
+        }),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (status, response, request) {
+            var obj = JSON.parse(request.responseText);
+            console.log('UpdateStream', obj);
+
+        }
+    });
+})();
+
+
 ///////////////////////////////////////////////////////
-$.ajax({
-    type: 'POST',
-    url: 'https://tv-server.trinity-tv.net/server/TvServerService/CloseStream.json',
-    data: JSON.stringify({
-        auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
-        stream_id: 2
-    }),
-    dataType: 'json',
-    contentType: "application/json",
-    success: function (status, response, request) {
-        var obj = JSON.parse(request.responseText);
-        console.log('CloseStream', obj);
-    }
-});
+(function closeStream() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://tv-server.trinity-tv.net/server/TvServerService/CloseStream.json',
+        data: JSON.stringify({
+            auth: 'bfae2c5899b64063dd9e4e3be9c96d30',
+            stream_id: 2
+        }),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (status, response, request) {
+            var obj = JSON.parse(request.responseText);
+            console.log('CloseStream', obj);
+
+            // if (data.ReopenStream) {
+            //     closeStream();
+            // }
+        }
+    });
+
+})();
+
 // $.ajax({
 //     beforeSend: function (xhr) {
 //         xhr.setRequestHeader ('Authorization', 'токен');
